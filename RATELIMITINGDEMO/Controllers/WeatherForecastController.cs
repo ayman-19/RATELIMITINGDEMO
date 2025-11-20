f43cab187b9c0743d6bd2b4143357ea2569aec88
ayman-19 <aymanroshdy909@email.com>
2025-11-20 16:47:10 +0200

    commit

--- RATELIMITINGDEMO/Controllers/WeatherForecastController.cs
+++ RATELIMITINGDEMO/Controllers/WeatherForecastController.cs
@@ -1,26 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace RATELIMITINGDEMO.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries =
-        [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        ];
+namespace RATELIMITINGDEMO.Controllers;
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries =
+    [
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching",
+    ];
+
+    /// <summary>
+    /// get weather forecast
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable
+            .Range(1, 5)
+            .Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
             })
             .ToArray();
-        }
     }
 }
--- RATELIMITINGDEMO/Program.cs
+++ RATELIMITINGDEMO/Program.cs
@@ -1,64 +1,63 @@
 ﻿using Microsoft.OpenApi;
 
-namespace RATELIMITINGDEMO
+namespace RATELIMITINGDEMO;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddScoped<RateLimitingMiddleware>();
+        builder.Services.AddMemoryCache();
+        builder.Services.AddSwaggerGen(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddScoped<RateLimitingMiddleware>();
-            builder.Services.AddMemoryCache();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc(
-                    "v1",
-                    new OpenApiInfo
+            options.SwaggerDoc(
+                "v1",
+                new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "My API",
+                    Description = "ASP.NET Core 10 Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
                     {
-                        Version = "v1",
-                        Title = "My API",
-                        Description = "ASP.NET Core 10 Web API",
-                        TermsOfService = new Uri("https://example.com/terms"),
-                        Contact = new OpenApiContact
-                        {
-                            Name = "Example Contact",
-                            Url = new Uri("https://example.com/contact"),
-                        },
-                        License = new OpenApiLicense
-                        {
-                            Name = "Example License",
-                            Url = new Uri("https://example.com/license"),
-                        },
-                    }
-                );
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license"),
+                    },
+                }
+            );
 
-                var xmlFilename =
-                    $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-            });
+            var xmlFilename =
+                $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
 
-            builder.Services.AddControllers();
-            builder.Services.AddOpenApi();
+        builder.Services.AddControllers();
+        builder.Services.AddOpenApi();
 
-            var app = builder.Build();
-            app.UseMiddleware<RateLimitingMiddleware>();
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-            app.UseSwagger();
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                options.RoutePrefix = "swagger";
-            });
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+        var app = builder.Build();
+        app.UseMiddleware<RateLimitingMiddleware>();
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
         }
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            options.RoutePrefix = "swagger";
+        });
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
     }
 }
